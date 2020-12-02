45616361fdcf92acad6cc54d745798417f697cd0
S Galarza <sgalarza4190@gmail.com>
2020-12-02 18:18:09 -0500

    adjusted the note removal function in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,17 +3,9 @@ const store = require("../db/store");
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", (req, res) => {
+  console.log("I am inside getNote()");
   store
     .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
-});
-
-router.post("/notes", (req, res) => {
-  // console.log("for req.body", req);
-  store
-    .addNote(req.body)
-    // res.json(true)
     .then((notes) => res.json(notes))
     .catch((err) => res.status(500).json(err));
 });
@@ -22,11 +14,24 @@ router.post("/notes", (req, res) => {
 // Use addNote in the store object
 // YOUR CODE HERE
 
+router.post("/notes", (req, res) => {
+  console.log("I am inside addNote()");
+  store
+    .addNote(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 // Use removeNote method in the store object
 // YOUR CODE HERE
 
+router.delete("/notes", (req, res) => {
+  console.log("I am inside removeNote()");
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+});
 
 
 module.exports = router;
